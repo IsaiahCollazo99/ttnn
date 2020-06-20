5002d3504de9081bde3fd133fa0feed53b78fa2e
kongnyc <bbsyoow@gmail.com>
2020-06-20 12:19:38 -0400

    handle sumbit handle # or not

--- tech-tonic/src/components/SearchBar.js
+++ tech-tonic/src/components/SearchBar.js
@@ -7,7 +7,11 @@ const SearchBar =()=>{
     const search = useInput("")
 
     const handleSubmit=(e)=>{
-                
+        if(search.value[0]==="#"){ //starting # hashtag search
+            console.log(search.value.slice(1))
+        }else{ //else search keyword
+            console.log(search.value)
+        }
     }
 
         return (
