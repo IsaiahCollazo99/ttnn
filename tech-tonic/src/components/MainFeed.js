3d261153b5fb03e2986af4e6ae674e8d8f8e6008
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-06-20 17:46:49 -0400

    create a handleSearch functoin

--- tech-tonic/src/components/MainFeed.js
+++ tech-tonic/src/components/MainFeed.js
@@ -10,31 +10,38 @@ const MainFeed = () => {
     const objectLength = ( object ) => {
         return Object.keys(object).length;
     }
+
+    const createQuery = ( dictionary ) => {
+        let query = "";
+            
+        for(let key in dictionary) {
+            const value = dictionary[key];
+            if(value) {
+                let queryEnd;
+                if(objectLength(userQueries)) {
+                    queryEnd = objectLength(userQueries) > 1 ? `${value} OR ` : value;
+                } else {
+                    queryEnd = `${value} OR `;
+                }
+                query += queryEnd;
+            } else {
+                continue;
+            }
+        }
+
+        let queryEnd = query.slice(-4);
+        if(queryEnd === " OR ") {
+            query = query.slice(0, -4);
+        }
+
+        return query;
+    }
     
-    const getTweets = async () => {
+    const getTweets = async ( search ) => {
         try {
             const dictionary = objectLength(userQueries) ? userQueries : defaultQueries;
-            let query = "";
             
-            for(let key in dictionary) {
-                const value = dictionary[key];
-                if(value) {
-                    let queryEnd;
-                    if(objectLength(userQueries)) {
-                        queryEnd = objectLength(userQueries) > 1 ? `${value} OR ` : value;
-                    } else {
-                        queryEnd = `${value} OR `;
-                    }
-                    query += queryEnd;
-                } else {
-                    continue;
-                }
-            }
-
-            let queryEnd = query.slice(-4);
-            if(queryEnd === " OR ") {
-                query = query.slice(0, -4);
-            }
+            const query = createQuery(dictionary);
 
             let encodedQuery = encodeURIComponent(query);
             
@@ -45,9 +52,15 @@ const MainFeed = () => {
         }
     }
 
+    const handleSearch = ( search ) => {
+        const newUserQueries = {...userQueries};
+        newUserQueries[search] = search;
+        setUserQueries({...newUserQueries});
+    }
+
     useEffect(() => {
         getTweets();
-    }, [])
+    }, [ userQueries ])
 
     const statusList = statuses.map(status => {
         return (
@@ -59,7 +72,7 @@ const MainFeed = () => {
 
     return (
         <>
-            <SearchBar />
+            <SearchBar handleSearch={handleSearch}/>
             <div>
                 <p>Feed Page</p>
                 {statusList}
