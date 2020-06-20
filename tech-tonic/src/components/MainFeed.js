f5bec40c8c03537cd54a6b8f90ab122f79ae4e59
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-06-20 15:25:49 -0400

    add proxy, and fetching into backend query for twitter search on initial load

--- tech-tonic/src/components/MainFeed.js
+++ tech-tonic/src/components/MainFeed.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const MainFeed =()=> {
+const MainFeed = () => {
+    const [ statuses, setStatuses ] = useState([]);
+    
+    const getTweets = async () => {
+        try {
+            let res = await axios.get("/api/tweets?search=javascript");
+            setStatuses(res.data.statuses);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getTweets();
+    }, [])
+
+    const statusList = statuses.map(status => {
         return (
-            <div>
-                <p>Feed Page</p>
+            <div className="status">
+                {status.text}
             </div>
         )
-    }
+    })
+
+    return (
+        <div>
+            <p>Feed Page</p>
+            {statusList}
+        </div>
+    )
+}
 
 
 export default MainFeed
