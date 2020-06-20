c431cd854553f8bf1cd9a5c9ac8349f7959cf1e8
kongnyc <bbsyoow@gmail.com>
2020-06-20 12:01:26 -0400

    add use input

--- tech-tonic/src/components/SearchBar.js
+++ tech-tonic/src/components/SearchBar.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import "../css/search.css"
+import { useInput } from "../util/useInput";
+
 
 const SearchBar =()=>{
+    const search = useInput("")
+
+    const handleSubmit=(e)=>{
+                
+    }
 
         return (
             <div>
-                <form>
+                <form onSubmit={handleSubmit}>
                 <button type="submit"><i className="search"></i></button>
-                <input type="text" placeholder="Search for #hashtag or keyword" name="search"></input>
+                <input type="text" placeholder="#hashtag or keyword" name="search" {...search}></input>
                 </form>
             </div>
         )
