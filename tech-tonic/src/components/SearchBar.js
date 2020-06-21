9b4b3aa6b35d50cf41332b6c6d14c852a1905d42
kongnyc <bbsyoow@gmail.com>
2020-06-21 14:22:11 -0400

    update icon name

--- tech-tonic/src/components/SearchBar.js
+++ tech-tonic/src/components/SearchBar.js
@@ -22,7 +22,7 @@ const SearchBar = ({ handleSearch })=>{
         return (
             <div>
                 <form className="search" onSubmit={handleSubmit}>
-                    <button type="submit"><i className="fa-search"></i></button>
+                    <button type="submit"><i className="fa fa-search"></i></button>
                     <input type="search" placeholder="#hashtag or keyword" {...searchObj} required />
                 </form>
             </div>
